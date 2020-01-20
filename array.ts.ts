4099bca7a0c119ad7453cc79788f9f99f12d3bd6
subinaBlock8 <59727891+subinaBlock8@users.noreply.github.com>
2020-01-20 17:41:00 +0530

    Add files via upload

---
+++ array.ts.ts
@@ -0,0 +1,14 @@
+var myArr:number[];
+myArr=[];//we can keep it empty OR
+myArr=[1,2,3];//we can declare the values
+
+
+//Methods can be used like
+myArr.push(4);
+myArr.pop();
+
+//Tuples
+var Arr :[number,boolean];
+Arr=[1,false];
+//So if we need to declare multiple data types we use Tuples.
+
